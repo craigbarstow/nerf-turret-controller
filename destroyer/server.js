358f59f67848dd2fe028d079defd3b46545b0a7f
Ben Calegari <bencalegari@gmail.com>
2015-03-14 13:54:19 -0400

    Fix index

--- destroyer/server.js
+++ destroyer/server.js
@@ -1,19 +1,5 @@
-// // modules =================================================
-// var express        	= require('express');
-// var app            	= express();
-
-// // configuration ===========================================
-// var port = process.env.PORT || 8000; // set our port
-
-// app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-
-// // routes ==================================================
-// require('./app/routes')(app); // configure our routes
-
-// // start app ===============================================
-// app.listen(port);										// startup our app at http://localhost:8080
-// console.log("It's goin down on port " + port); 			// shoutout to the user
-// exports = module.exports = app; 						// expose app
+// FFMPEG Command to Run Streaming
+// ffmpeg -f qtkit -i "0" -s 640x480 -f mpeg1video -b 10k -r 20 http://localhost:8082/password/640/480
 
 
 if( process.argv.length < 3 ) {
